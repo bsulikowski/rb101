18bb5e6258d7c48ad4e4aefd45e91b0142fff25a
Ben Sulikowski <BenSulikowski@gmail.com>
2022-03-11 11:45:50 -0500

    Add more practice problems

---
+++ lesson_3/practice_problems_hard_1/question1.rb
@@ -0,0 +1,9 @@
+# What do you expect to happen when the greeting variable is referenced in the last line of the code below?
+
+if false
+  greeting = "hello world"
+end
+
+greeting
+
+# We will get an error code. The if statement will never execute so the greeting variable is not assigned.
---
+++ lesson_3/practice_problems_medium_2/question3.rb
@@ -0,0 +1,23 @@
+# Let's call a method, and pass both a string and an array as arguments and see
+# how even though they are treated in the same way by Ruby, the results can be different.
+
+# Study the following code and state what will be displayed...and why:
+
+def tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param << "rutabaga"
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
+
+# "My string looks like this now: 'pumpkins'"
+# "My array looks like this now: 'pumpkins'
+# 'rutabaga'"
+
+# The string param variable was reassigned in the method, pointing to a new object id
+# the array was mutated, still pointing to the same object as the my_array variable
