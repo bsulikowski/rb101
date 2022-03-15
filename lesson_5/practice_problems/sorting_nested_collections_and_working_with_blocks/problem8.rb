8218a39d74fe315bf46283aa47950964cece0772
Ben Sulikowski <BenSulikowski@gmail.com>
2022-03-15 17:28:07 -0400

    Add more lesson work

---
+++ lesson_5/practice_problems/sorting_nested_collections_and_working_with_blocks/problem8.rb
@@ -0,0 +1,14 @@
+# Using the each method, write some code to output all of the vowels from the strings.
+
+VOWELS = %w(a e i o u)
+
+hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
+vowels_hsh = {}
+
+hsh.each do |_, value|
+  value.each do |str|
+    str.chars.each do |char|
+      puts char if VOWELS.include?(char)
+    end
+  end
+end
